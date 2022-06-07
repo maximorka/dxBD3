ccab0939fda6fcc294b59472a21802f0368cf5ca
maximorka <maximorka@gmail.com>
2022-06-07 12:19:37 +0300

    first commit

---
+++ 3.2/1.sql
@@ -0,0 +1,21 @@
+--1. Добавить разработчикам поле (salary - зарплата). 
+
+--1
+ALTER TABLE developer
+ADD salary INTEGER ;
+
+UPDATE developer
+SET salary = 3600
+WHERE id = 1;
+
+UPDATE developer
+SET salary = 2200
+WHERE id = 2;
+
+UPDATE developer
+SET salary = 2200
+WHERE id = 3;
+
+UPDATE developer
+SET salary = 4600
+WHERE id = 4;
